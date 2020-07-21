b0cfd5c9912416f161705f05c34da300559e8a1f
Rami <cashamir@gmail.com>
2020-07-21 09:18:26 +0200

    #13 - Hooks with Context (using useContext)
    using useContext hook in stateless component instead of instead of static contextType in class component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div>
-        <div
-          className='book-list'
-          style={{ color: theme.syntax, background: theme.bg }}
-        >
-          <ul>
-            <li style={{ background: theme.ui }}>The way of kings</li>
-            <li style={{ background: theme.ui }}>The way of the wind</li>
-            <li style={{ background: theme.ui }}>The final empire</li>
-          </ul>
-        </div>
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div>
+      <div
+        className='book-list'
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>The way of kings</li>
+          <li style={{ background: theme.ui }}>The way of the wind</li>
+          <li style={{ background: theme.ui }}>The final empire</li>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BookList;
